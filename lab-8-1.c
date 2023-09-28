565b9cef0c395a5d2acbd55e215a60716eff7776
Akshat Nagpal <69424903+OverRancid@users.noreply.github.com>
2023-09-28 13:56:38 +0530

    hash tables

---
+++ lab-8-1.c
@@ -0,0 +1,159 @@
+//    ⠘  AKSHAT NAGPAL                ⡜
+//    ⠑⡀           2022MCB1255      ⡔⠁⠀⠀⠀
+// ⠀⠀⠀⠀⠈⠢⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀  ⣀⠴⠊⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⢀⣀⣀⣀⣀⣀⡀⠤⠄⠒⠈⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠘⣀⠄⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠉⢈⠩⢙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⢋⠠⠀⠀⠨⠐⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢐⠐⠌⡌⢄⢐⢈⠔⡝⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⡏⠉⡀⠐⡀⢁⠈⠐⠱⠑⡑⠈⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣿⢗⠀⠀⠐⡠⡛⠔⡁⢜⡔⡬⢎⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⡿⠡⠀⠀⠀⠀⠂⠁⠀⠄⢂⠈⠂⢂⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⡿⢟⠩⠐⡀⠀⠀⠀⠐⠐⠁⠓⠒⠒⢀⠁⢐⢝⢟⢿⣿⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⠫⠡⠡⠨⢀⠂⠠⠀⠀⢁⠑⡱⠛⠗⡓⢂⠠⢸⢸⢨⠣⡝⣻⣿⣿⣿⣿⣿⣿
+// ⣿⢏⢐⢁⠊⢌⠐⡈⠄⠠⠀⠀⠀⠀⠁⠑⠈⠀⢄⢕⠸⡨⠪⡪⡘⣻⣿⡿⣿⣿⣿
+// ⣿⢂⠂⡂⠅⡂⠅⡐⠨⢐⠐⠠⠠⡀⢄⠠⡠⡡⡱⡐⠕⢌⢊⢆⢣⢒⠽⢿⣿⣿⣿
+// ⠣⢂⠂⠄⠡⠐⠐⠈⠌⡐⠨⡈⠢⠨⡂⢌⢂⠆⡪⠨⡊⠂⡂⠢⢡⣢⣣⡣⣍⢿⣿
+// ⠨⢂⢂⠁⡀⠀⠀⠁⠐⠈⠐⠈⢈⠈⠐⡀⠄⠁⠌⠈⠔⣄⡀⠠⡑⡂⠆⠢⢂⠑⠽
+// ⡨⠐⠀⠀⠀⢠⡎⡀⠀⠀⠄⠈⡀⠌⠐⠠⠈⠄⡁⠂⡀⡫⠑⣑⠀⢂⠌⠄⢕⠀⠨
+// ⠺⡪⠢⡀⠀⠞⢇⢂⠀⠂⡀⠠⠀⠄⠁⠌⠨⠀⢄⠢⡁⢂⢿⡟⡀⠀⠈⠈⡀⠂⣰
+// ⢀⢀⠀⠄⠀⠀⡐⠀⡈⠄⡐⠅⡊⠌⢌⠄⡕⡑⡁⢂⠂⢂⠸⣿⡄⠀⠈⣠⣴⣿⣿
+// ⢐⠔⠠⠀⠀⡐⠠⢈⠢⢑⠄⠑⢈⠊⡂⡱⢁⣂⢌⢔⢌⢄⠀⠹⢀⣺⡿⣟⢿⣿⣿
+// ⢀⠡⠁⠂⠐⠠⠈⠄⢈⠠⢈⢢⡣⣗⠕⠄⣕⢮⣞⣞⣗⣯⢯⡷⡴⣹⡪⣷⣿⣿⣿
+// ⠊⠄⠠⠠⠡⠈⠠⢐⠠⡊⡎⣗⢭⢐⠹⡹⣮⡳⡵⣳⣻⢾⣻⣽⣻⣺⣺⣽⣿⣿⣿
+// ⣨⣾⢐⠰⠐⠅⡂⡂⢕⢜⢜⢵⢹⢑⢔⠨⢘⠸⡹⡵⣯⣻⢽⣳⣻⣺⢞⡿⣿⣿⣿
+// ⣿⣿⡔⠠⢈⠐⠐⢠⢱⢸⢸⢸⢸⠰⡡⢘⢔⢕⠝⢮⣳⢽⢝⡾⡵⡯⣏⠯⣿⣿⣿
+// ⣿⣿⣗⢅⢢⠠⠡⠢⡱⡑⡕⡕⢅⠣⡊⢨⢪⡣⡣⡂⡬⡳⢽⢽⢽⢽⣞⣧⠙⣿⣿
+// ⡻⣿⡯⡪⠢⡡⠡⢑⢌⠪⡪⡊⠆⢌⠪⢐⢕⢱⢱⢱⢱⢱⢙⢮⡫⡟⣞⢮⣳⠙⣿
+// ⠊⣿⣯⠪⡊⠄⢅⠂⢂⠁⢇⢇⢃⠂⢕⠐⠌⡲⡰⡡⣇⠇⢇⢕⠪⠉⠂⠅⠂⡑⠹
+// ⣸⢿⣳⢱⠨⡐⡽⡿⡶⡾⡬⡢⢂⠅⡢⢡⣌⠐⠈⢎⢎⢎⢔⠠⠡⠠⠠⠡⡁⡂⠡
+// ⡯⡯⡇⢅⠕⠠⢱⢹⡙⢮⢹⠨⡂⡂⢇⠌⠮⡳⠅⡂⢕⠡⡑⠠⢁⢁⣡⣡⣢⣶⣿
+// ⣗⢽⢌⡢⡡⡡⡸⡢⡣⡣⡱⡑⠔⡈⢎⢆⢂⠂⠅⣢⡳⣽⡐⢅⢂⣊⣿⣿⣿⣿⣿
+// ⣯⢯⢷⢽⢮⢯⣺⣪⢞⡮⣳⢘⠔⢌⢜⣞⣖⣮⣻⢮⣯⢷⣿⣻⣿⣿⣿⣿⣿⣿⣿
+// ⣿⣿⣿⣿⣿⣿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣾⣷⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+
+#include <stdio.h>         //Parental Advisory
+#include <stdlib.h>       //Explicit Content
+
+typedef struct doubly_linked_list{      //structure of doubly linked list
+    int value;
+    struct doubly_linked_list* prev;
+    struct doubly_linked_list* next;
+}node;
+
+int m;                                              //user initialized global hashing constant
+node **T;                                           //global hash table
+void checkMalloc(void *ptr){                        //checks if malloc worked
+    if(ptr==NULL){
+        printf("malloc failed bruh.\n");
+        exit(-1);
+    }
+}
+int h(int k){                                       //hashing function
+    return (k%m+m)%m;                           //+m prevents negative hash values
+}
+
+void dll_insert_head(node* *root, int value){        //insert at head of doubly linked list
+    node *new = (node *)malloc(sizeof(node));
+    checkMalloc(new);
+    new->value = value;
+    new->prev = NULL;
+    new->next = *root;
+    if(*root)
+        (*root)->prev = new;
+    *root = new;
+}
+node* dll_search(node* root, int value){             //searches for value in doubly linked list
+    while(root && root->value!=value)
+        root = root->next;
+    return root;
+}              
+void dll_delete(node* *root, int value){             //deletes value from doubly linked list
+    if((*root)->value==value){          //deleting first value
+        node *temp = *root;
+        *root = (*root)->next;
+        if((*root)->next)
+            (*root)->next->prev = *root;
+        free(temp); 
+        return;
+    }
+    node* to_delete = dll_search(*root, value);    //abstraction moment
+    if(!to_delete)
+        return;
+    if(to_delete->prev)
+        to_delete->prev->next = to_delete->next;
+    if(to_delete->next)
+        to_delete->next->prev = to_delete->prev;
+    free(to_delete);
+}
+void dll_show(node* root){                           //prints elements of doubly linked list
+    if(!root){
+        printf("NULL");
+    }
+    while(root){
+        printf("%d ", root->value);
+        root = root->next;
+    }
+    printf("\n");
+}
+
+void hash_insert(int value){                        //insert element into hash table
+    dll_insert_head(&T[h(value)], value);
+}
+void hash_delete(int value){                        //delete element from hash table
+    dll_delete(&T[h(value)], value);
+}
+void hash_search(int value){                        //search for element in hash table
+    node* found = dll_search(T[h(value)], value);
+    if(found)
+        printf("Searched element %d is found in slot number %d\n", value, h(value));
+    else
+        printf("Searched element %d not found\n", value);
+}
+void hash_display(){                                //display the entire hash table
+    for(int i=0; i<m; i++){
+        printf("index %d: ", i);
+        dll_show(T[i]);
+    }
+}
+
+int main(void){
+    printf("Enter hash table size m: ");
+    scanf("%d", &m);
+
+    T = (node**)malloc(m*sizeof(node*));
+    checkMalloc(T);
+    for(int i=0; i<m; i++){             //Prevent garbage values.
+        T[i] = NULL;
+    }
+
+    printf("1. Insert\n2. Delete\n3. Search\n4. Display\n5. Quit\n");
+    int choice, n;
+    for(;;){
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        switch(choice){
+        case 1:
+            printf("Enter value to insert: ");
+            scanf("%d", &n);
+            hash_insert(n);
+            break;
+        case 2:
+            printf("Enter value to delete: ");
+            scanf("%d", &n);
+            hash_delete(n);
+            break;
+        case 3:
+            printf("Enter value to be searched: ");
+            scanf("%d", &n);
+            hash_search(n);
+            break;
+        case 4:
+            hash_display();
+            break;
+        case 5:
+            return 0;
+        }
+    }
+}
+
